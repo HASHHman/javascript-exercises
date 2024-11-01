603c0efd5920f3d24012162431f2cbd10c24135b
HASHHman <SAHHH.ir@gmail.com>
2024-11-01 08:20:38 +0330

    Solve the exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  const unRoundedCel = (F - 32) * 5 / 9;
+  const roundedCel = Math.round(unRoundedCel * 10) / 10;
+  return roundedCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  const unRoundedF = cel * 9 / 5 + 32;
+  const roundedF = Math.round(unRoundedF * 10) / 10;
+  return roundedF;
 };
 
 // Do not edit below this line
