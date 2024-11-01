2ed8330734290976dc8af4f6d07aba33ddf9aa48
HASHHman <SAHHH.ir@gmail.com>
2024-11-01 07:39:11 +0330

    Solve the exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splittedString = string.split('');
+    let outputString = splittedString.reverse().join('');
+    return outputString;
 };
 
 // Do not edit below this line
