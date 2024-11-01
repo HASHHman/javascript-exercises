863638533e801ca42ac88f764199b809fc8a1087
HASHHman <SAHHH.ir@gmail.com>
2024-11-01 07:37:39 +0330

    Solve the exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...unwantedValues) {
+    function isWanted(item) {
+        for (const unwantedValue of unwantedValues) {
+            if (item === unwantedValue) {
+                return false;
+                break
+            };
+        }
+        return true;
+    }
+    const resultArray = array.filter(isWanted);
+    //console.log(resultArray);
+    return resultArray;
 };
 
 // Do not edit below this line
