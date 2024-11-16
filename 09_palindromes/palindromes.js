a4e59705f55af8b1b0e8cac7b829f78c91f04447
HASHHman <SAHHH.ir@gmail.com>
2024-11-16 10:05:40 +0330

    Solve the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (s = '') {
+  const formattedString = s.replaceAll(/\W/g, '').toLowerCase();
+  const originalArray = formattedString.split('');
+  const reversedArray = originalArray.toReversed();
+  return originalArray.every((char, index) => reversedArray[index] === char);
 };
 
 // Do not edit below this line
