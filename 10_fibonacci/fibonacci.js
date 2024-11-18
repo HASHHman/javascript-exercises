a4eb1b364f945d1e31bae4ef05d17f6cd59e842d
HASHHman <SAHHH.ir@gmail.com>
2024-11-18 16:52:32 +0330

    Solve the fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,10 +1,21 @@
-const fibonacci = function(n) {
-  const arr = Array(n);
-  const lastTwoNums = are.reduce(lastTwoNums => {
-    const currNum = lastTwoNums[0] + lastTwoNums[1];
-    return [lastTwoNums[1], currNum];
-  }, [1, 1]);
-  return lastTwoNums[1];
+const fibonacci = function (n) {
+  n = parseInt(n);
+  if (n < 0) {
+    return "OOPS"
+  }
+	else if (n > 1) {
+		const arr = Array.from(Array(n - 2));
+		const lastTwoNums = arr.reduce(
+			(lastTwoNums) => {
+				const currNum = lastTwoNums[0] + lastTwoNums[1];
+				return [lastTwoNums[1], currNum];
+			},
+			[1, 1]
+		);
+		return lastTwoNums[1];
+	} else if (n === 1) {
+		return 1;
+	} else return 0;
 };
 
 // Do not edit below this line
