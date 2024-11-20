51dd65de1e9e3cf54ca19ab269a5deb49847dfd4
HASHHman <SAHHH.ir@gmail.com>
2024-11-20 08:39:31 +0330

    Solve the 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+	const ages = people.map((person) => (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth);
+	console.log(ages);
+	const sortedAges = ages.toSorted((a, b) => (a > b ? -1 : 1));
+	console.log(sortedAges);
+	const oldestPersonId = ages.indexOf(sortedAges[0]);
+	console.log(oldestPersonId);
+	const oldestPerson = people[oldestPersonId];
+	console.log(oldestPerson);
+	return oldestPerson;
 };
 
 // Do not edit below this line
